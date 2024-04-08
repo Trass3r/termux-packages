66f837c72051e35225e869faa38840f2a1311751
Trass3r <trass3r@gmail.com>
2024-04-08 22:41:09 +0200

    add libdebuginfod-dev

--- packages/gdb/build.sh
+++ packages/gdb/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_VERSION="15.2"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gdb/gdb-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=83350ccd35b5b5a0cba6b334c41294ea968158c573940904f00b92f76345314d
 TERMUX_PKG_DEPENDS="guile, libc++, libexpat, libgmp, libiconv, liblzma, libmpfr, libthread-db, ncurses, python, readline, zlib, zstd"
+TERMUX_PKG_BUILD_DEPENDS="libdebuginfod-dev"
 TERMUX_PKG_BREAKS="gdb-dev"
 TERMUX_PKG_REPLACES="gdb-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
