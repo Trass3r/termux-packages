03b8ee59bd68393a297ad7b3a44c8e1401a2b375
Trass3r <trass3r@gmail.com>
2025-03-25 21:03:47 +0100

    upgrade hangover to 10.4

--- x11-packages/hangover-wine/build.sh
+++ x11-packages/hangover-wine/build.sh
@@ -3,16 +3,15 @@ TERMUX_PKG_DESCRIPTION="A compatibility layer for running Windows programs (Hang
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.OLD, COPYING.LIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=10.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=10.4
 _REAL_VERSION="${TERMUX_PKG_VERSION/\~/-}"
 TERMUX_PKG_SRCURL=(
 	https://github.com/AndreRH/wine/archive/refs/tags/hangover-$_REAL_VERSION.tar.gz
 	https://github.com/AndreRH/hangover/releases/download/hangover-$_REAL_VERSION/hangover_${_REAL_VERSION}_ubuntu2004_focal_arm64.tar
 )
 TERMUX_PKG_SHA256=(
-	0633f3fd1991f742761ccfef5ed32814d9aab49a5c49fb5e4a48b58c15669623
-	848f299d35ef73151486841dd2f1dc453027327632f785643ad83adf7ed9219c
+	055a11ad91d27fca74cd80d6cf94b03deb843aa9d07b462e7fb46365203b8b60
+	944c1c8e68bd7d4f61a2e9a89e7460912bf3ed85db66fce64c8f1aaf184dc8ad
 )
 TERMUX_PKG_DEPENDS="fontconfig, freetype, krb5, libandroid-spawn, libc++, libgmp, libgnutls, libxcb, libxcomposite, libxcursor, libxfixes, libxrender, mesa, opengl, pulseaudio, sdl2, vulkan-loader, xorg-xrandr"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
