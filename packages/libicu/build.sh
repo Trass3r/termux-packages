52ba9326696f191f848428a75834b642d97e9b08
Trass3r <trass3r@gmail.com>
2026-01-19 13:48:51 +0100

    Bump libicu version from 78.1 to 78.2

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -7,9 +7,9 @@ TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 # Never forget to always bump revision of reverse dependencies and rebuild them
 # when bumping "major" version.
-TERMUX_PKG_VERSION="78.1"
+TERMUX_PKG_VERSION="78.2"
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION}/icu4c-${TERMUX_PKG_VERSION}-sources.tgz
-TERMUX_PKG_SHA256=6217f58ca39b23127605cfc6c7e0d3475fe4b0d63157011383d716cb41617886
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_DEPENDS="libc++"
