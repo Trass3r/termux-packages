a14b1d0b91d0c7b5011f6b40fb69b7aff5b30514
Trass3r <trass3r@gmail.com>
2026-01-19 23:35:59 +0100

    add termux_step_configure override

--- packages/vulkan-bcn-layer/build.sh
+++ packages/vulkan-bcn-layer/build.sh
@@ -8,3 +8,7 @@ TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
+
+termux_step_configure() {
+	return;
+}
