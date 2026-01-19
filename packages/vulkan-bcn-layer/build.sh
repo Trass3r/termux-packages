1587545185769070af94afd15b3e603d685f8837
Trass3r <trass3r@gmail.com>
2026-01-20 00:59:25 +0100

    fix git branch

--- packages/vulkan-bcn-layer/build.sh
+++ packages/vulkan-bcn-layer/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@Pipetto-crypto"
 TERMUX_PKG_VERSION="1.0"
 TERMUX_PKG_SRCURL=git+https://github.com/Pipetto-crypto/bcn_layer.git
+TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
