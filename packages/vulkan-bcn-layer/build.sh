c9d6864d9563197abc69a8330c820fed213c0241
Trass3r <trass3r@gmail.com>
2026-01-19 23:11:44 +0100

    add BCn vulkan emulation layer

---
+++ packages/vulkan-bcn-layer/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Pipetto-crypto/bcn_layer
+TERMUX_PKG_DESCRIPTION="Vulkan BCn Emulation Layer"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@Pipetto-crypto"
+TERMUX_PKG_VERSION="1.0"
+TERMUX_PKG_SRCURL=https://github.com/Pipetto-crypto/bcn_layer/archive/refs/heads/main.tar.gz
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
+TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
