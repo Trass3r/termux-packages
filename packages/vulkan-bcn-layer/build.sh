08b43fbbc6c8e80aafbb705e51052131352b775b
Trass3r <trass3r@gmail.com>
2026-01-20 02:31:45 +0100

    fix tab

--- packages/vulkan-bcn-layer/build.sh
+++ packages/vulkan-bcn-layer/build.sh
@@ -12,6 +12,6 @@ TERMUX_PKG_BUILD_DEPENDS="libxcb, libxrandr, vulkan-headers, vulkan-utility-libr
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 
 termux_step_configure() {
-    sudo apt install -y glslc
+	sudo apt install -y glslc
 	return;
 }
