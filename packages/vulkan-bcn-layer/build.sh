5c288d178f114c740d5744007fd29e6997310da8
Trass3r <trass3r@gmail.com>
2026-01-20 01:46:36 +0100

    TERMUX_PKG_BUILD_IN_SRC=true

--- packages/vulkan-bcn-layer/build.sh
+++ packages/vulkan-bcn-layer/build.sh
@@ -6,8 +6,9 @@ TERMUX_PKG_VERSION="1.0"
 TERMUX_PKG_SRCURL=git+https://github.com/Pipetto-crypto/bcn_layer.git
 TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
+TERMUX_PKG_BUILD_DEPENDS="libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 
 termux_step_configure() {
