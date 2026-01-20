fea1ec2bd79af51c7db9e364c18401c41c4a8e87
Trass3r <trass3r@gmail.com>
2026-01-20 02:39:38 +0100

    no like sudo

--- packages/vulkan-bcn-layer/build.sh
+++ packages/vulkan-bcn-layer/build.sh
@@ -8,10 +8,9 @@ TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
+TERMUX_PKG_BUILD_DEPENDS="glslc, libxcb, libxrandr, vulkan-headers, vulkan-utility-libraries"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 
 termux_step_configure() {
-	sudo apt install -y glslc
 	return;
 }
